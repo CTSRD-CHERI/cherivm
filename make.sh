7fb7345b43f404d37583d3d47b48b5792e55bf52
David Brazdil <david.brazdil@gmail.com>
2014-04-13 14:40:26 +0000

    Make script builds given target or 'install'

--- make.sh
+++ make.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
  
 source chericc.inc
-  
+
+if [[ x"$1" == x ]]; then
+    TARGET="install"
+else
+    TARGET="$1"
+fi
+
 for x in "$DIR_SRC/"*; do
     pushd "$x" > /dev/null
-        gmake $1 || exit
-        gmake install || exit
+        gmake "$TARGET" || exit
     popd > /dev/null
 done
 
