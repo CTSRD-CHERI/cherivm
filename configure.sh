e64f18a32bee7fd2a12396dd664a752b91ac5ba7
David Brazdil <david.brazdil@gmail.com>
2014-04-17 20:02:16 +0000

    Reenabled Classpath, building against shared libcheri

--- configure.sh
+++ configure.sh
@@ -7,22 +7,22 @@ if [ "$1" == "-d" ]; then
     JAMVM_DEBUG="--enable-trace"
 fi
 
-#echo "Configuring GNU Classpath..."
-#pushd "$DIR_CLASSPATH" > /dev/null
-#    try_to_run ./configure \
-#      --prefix="$DIR_TARGET" \
-#      --target=mips4-unknown-freebsd \
-#      --host=x86_64-unknown-freebsd10.0 \
-#      --with-sysroot="$DIR_CHERISDK" \
-#      --enable-default-preferences-peer=file \
-#      --disable-plugin \
-#      --disable-gtk-peer \
-#      --disable-gconf-peer \
-#      --disable-qt-peer \
-#      --without-x \
-#      --disable-gjdoc \
-#      --disable-examples 
-#popd > /dev/null
+echo "Configuring GNU Classpath..."
+pushd "$DIR_CLASSPATH" > /dev/null
+    try_to_run ./configure \
+      --prefix="$DIR_TARGET" \
+      --target=mips4-unknown-freebsd \
+      --host=x86_64-unknown-freebsd10.0 \
+      --with-sysroot="$DIR_CHERISDK" \
+      --enable-default-preferences-peer=file \
+      --disable-plugin \
+      --disable-gtk-peer \
+      --disable-gconf-peer \
+      --disable-qt-peer \
+      --without-x \
+      --disable-gjdoc \
+      --disable-examples 
+popd > /dev/null
 
 echo "Configuring JamVM..."
 pushd "$DIR_JAMVM" > /dev/null
