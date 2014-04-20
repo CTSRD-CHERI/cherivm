19526cc50d5263165ebbb81db9c81764a2b2dbd2
David Brazdil <david.brazdil@gmail.com>
2014-04-20 21:45:03 +0000

    Build scripts don't build GNU Classpath

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
  
 source chericc.inc
 
-for x in "$DIR_LIBCHERIJNI" "$DIR_CLASSPATH" "$DIR_JAMVM"; do
+for x in "$DIR_LIBCHERIJNI" "$DIR_JAMVM"; do
     pushd "$x" > /dev/null
         echo "Reconfiguring $x..."
         try_to_run autoreconf -fvi 
